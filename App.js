ec0b5cb00d400b102cc608fc2cbb0627355e9948
David Code Howard <david@techmatters.org>
2022-12-05 12:29:01 -0500

    feat: Get geolocation info

--- App.js
+++ App.js
@@ -1,16 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { StyleSheet, ScrollView, Text, TextInput, View } from "react-native";
+import Geolocation from "@react-native-community/geolocation";
 
 export default function App() {
   const [name, setName] = useState("");
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition((info) => {
+      setLocation(info.coords);
+    });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Please input the scene name</Text>
-      <TextInput style={styles.nameInput} value={name} onChangeText={setName} />
-      {name && <Text>Scene {name}</Text>}
-      <StatusBar style="auto" />
-    </View>
+    <React.StrictMode>
+      <View style={styles.container}>
+        <Text>Please input the scene name</Text>
+        <TextInput
+          style={styles.nameInput}
+          value={name}
+          onChangeText={setName}
+        />
+        <View>
+          {(name && (
+            <Text>
+              <p>Scene</p>
+              <p>{name}</p>
+            </Text>
+          )) || <Text></Text>}
+          {location ? (
+            <Text>
+              You are at: {location.latitude.toLocaleString()},{" "}
+              {location.longitude.toLocaleString()}
+            </Text>
+          ) : (
+            <Text>Fetching location...</Text>
+          )}
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    </React.StrictMode>
   );
 }
 
