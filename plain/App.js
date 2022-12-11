c8fca4b30d40d2341776a2103e07a1d1bd179941
David Code Howard <me@davidcode.ca>
2022-12-11 18:31:23 -0500

    feat: Add camera support
    The styling is still not so good, needs to be fixed up a bit.

--- plain/App.js
+++ plain/App.js
@@ -6,9 +6,11 @@
  * @flow strict-local
  */
 
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef, useCallback} from 'react';
 import type {Node} from 'react';
 import {
+  Button,
+  Image,
   PermissionsAndroid,
   SafeAreaView,
   ScrollView,
@@ -27,31 +29,40 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import {Camera, useCameraDevices} from 'react-native-vision-camera';
+
 import Geolocation from '@react-native-community/geolocation';
 
-const requestLocationPermission = async () => {
+const requestPermissions = async (permissionName, options) => {
   try {
-    const granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-      {
-        title: 'Example App Location Permission',
-        message: 'Cool Photo App needs access your location ',
-        buttonNeutral: 'Ask Me Later',
-        buttonNegative: 'Cancel',
-        buttonPositive: 'OK',
-      },
-    );
-    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      console.log('You can use the camera');
-      return true;
-    } else {
-      console.log('Camera permission denied');
-      return false;
-    }
+    const granted = await PermissionsAndroid.request(permissionName, options);
+    return granted === PermissionsAndroid.RESULTS.GRANTED;
   } catch (err) {
     console.warn(err);
     return false;
   }
+};
+
+const requestLocationPermission = () =>
+  requestPermissions(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION, {
+    title: 'Example App Location Permission',
+    message: 'Cool Photo App needs access your location ',
+    buttonNeutral: 'Ask Me Later',
+    buttonNegative: 'Cancel',
+    buttonPositive: 'OK',
+  });
+
+const requestCameraPermission = () =>
+  requestPermissions(PermissionsAndroid.PERMISSIONS.CAMERA, {
+    title: 'Example App Camera Permission',
+    message: 'Cool Photo App needs to use your camera ',
+    buttonNeutral: 'Ask Me Later',
+    buttonNegative: 'Cancel',
+    buttonPositive: 'OK',
+  });
+
+const CaptureButton = ({captureMedia}) => {
+  return <Button title="Take Picture" onPress={captureMedia}></Button>;
 };
 
 /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
@@ -84,21 +95,38 @@ const Section = ({children, title}): Node => {
 
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
-  const [hasPermissions, setPermissions] = useState(false);
+  const [hasLocationPermission, setLocationPermission] = useState(false);
+  const [hasCameraPermission, setCameraPermission] = useState(false);
   const [location, setLocation] = useState(null);
+  const [photo, setPhoto] = useState(null);
+  const camera = useRef();
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const devices = useCameraDevices('wide-angle-camera');
+  const device = devices.back;
+
+  const captureMedia = useCallback(
+    args => {
+      camera.current
+        .takePhoto()
+        .then(setPhoto)
+        .then(() => console.debug(photo.path));
+    },
+    [camera],
+  );
+
   useEffect(() => {
     requestLocationPermission().then(granted => {
-      setPermissions(granted);
+      setLocationPermission(granted);
       if (!granted) {
         return;
       }
       Geolocation.getCurrentPosition(info => setLocation(info.coords));
     });
+    requestCameraPermission().then(setCameraPermission);
   }, []);
 
   return (
@@ -115,12 +143,40 @@ const App: () => Node = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          {hasPermissions && location ? (
+          {hasLocationPermission && location ? (
             <Text>
               You are located at: {location.latitude}, {location.longitude}
             </Text>
           ) : (
             <Text>I don't know where you are located!</Text>
+          )}
+        </View>
+        <View
+          style={{
+            height: 400,
+          }}>
+          {hasCameraPermission && device ? (
+            <>
+              <Camera
+                style={StyleSheet.absoluteFill}
+                device={device}
+                isActive={true}
+                ref={camera}
+                photo={true}
+              />
+              <CaptureButton
+                style={StyleSheet.capture}
+                captureMedia={captureMedia}
+              />
+            </>
+          ) : (
+            <Text>No permissions to show camera</Text>
+          )}
+          {photo && (
+            <Image
+              source={{uri: 'file://' + photo.path}}
+              style={{height: 200, width: 200}}
+            />
           )}
         </View>
       </ScrollView>
@@ -144,6 +200,10 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  capture: {
+    height: 200,
+    width: '100%',
   },
 });
 
